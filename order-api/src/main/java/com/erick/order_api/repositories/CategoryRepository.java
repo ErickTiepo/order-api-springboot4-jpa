d3d434b8b706af5dc7d09b85e75430a8f650e095
erick <ericktiepo337@gmail.com>
2026-09-02 14:39:32 -0300

    Category

---
+++ order-api/src/main/java/com/erick/order_api/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.erick.order_api.repositories;
+
+import com.erick.order_api.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
