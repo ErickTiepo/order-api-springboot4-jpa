81e4310419ec7675eeffc8ddbccbff4915d64ae9
erick <ericktiepo337@gmail.com>
2026-09-02 13:16:34 -0300

    Service layer, component registration

--- order-api/src/main/java/com/erick/order_api/repositories/UserRepository.java
+++ order-api/src/main/java/com/erick/order_api/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.erick.order_api.repositories;
 import com.erick.order_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
