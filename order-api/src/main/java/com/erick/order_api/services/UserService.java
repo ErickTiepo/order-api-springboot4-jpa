8e853a4d7cf19afe1a73471f7b0b49c1bbfea531
erick <ericktiepo337@gmail.com>
2026-09-05 09:14:37 -0300

    Exception handling - update

--- order-api/src/main/java/com/erick/order_api/services/UserService.java
+++ order-api/src/main/java/com/erick/order_api/services/UserService.java
@@ -4,6 +4,7 @@ import com.erick.order_api.entities.User;
 import com.erick.order_api.repositories.UserRepository;
 import com.erick.order_api.services.exceptions.DatabaseException;
 import com.erick.order_api.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 
@@ -45,9 +46,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
